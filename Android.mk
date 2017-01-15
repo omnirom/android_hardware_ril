6a7aafd2b962cc77ade2dc67602315af6a681444
Jake Whatley <jacob.whatley91@gmail.com>
2017-01-15 18:43:15 -0500

    Revert "ril: CAF bringup"
    This reverts commit 25ebd2b97a7444d81b80f7ea588747722c4c870c.
    
    Change-Id: I6b521348eb8b636264e6a6efecaddecf45023151

--- Android.mk
+++
@@ -1,8 +0,0 @@
-ifeq ($(TARGET_RIL_VARIANT),)
-    RIL_ROOT := $(call my-dir)
-
-    include $(RIL_ROOT)/libril/Android.mk
-    include $(RIL_ROOT)/librilutils/Android.mk
-    include $(RIL_ROOT)/reference-ril/Android.mk
-    include $(RIL_ROOT)/rild/Android.mk
-endif
