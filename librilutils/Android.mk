91fafb10f82e60f568c61e5c601102ad66a3bf8a
Jacob Whatley <jake@omnirom.org>
2017-11-01 18:58:42 +0000

    Revert "Move librilutils to vendor."
    This reverts commit 323b6728c28c4837b07201c534c5af23a7756005.
    
    Change-Id: I3d694a775d9064501fe55d1855ba732ee45eb7a7

--- librilutils/Android.mk
+++ librilutils/Android.mk
@@ -17,7 +17,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 LOCAL_PROTOC_OPTIMIZE_TYPE := nanopb-c-enable_malloc
 
 LOCAL_MODULE:= librilutils
-LOCAL_VENDOR_MODULE:= true
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -40,7 +39,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 LOCAL_PROTOC_OPTIMIZE_TYPE := nanopb-c-enable_malloc
 
 LOCAL_MODULE:= librilutils_static
-iLOCAL_VENDOR_MODULE:= true
 
 include $(BUILD_STATIC_LIBRARY)
 
